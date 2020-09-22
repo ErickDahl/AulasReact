3e975e07c614d2db4e121ed480a38ad02a8006ad
Erick Dahl <erickdahl1@gmail.com>
2020-09-22 12:14:10 -0300

    aula

--- Aula02/serratec/src/pages/Dashboard/index.js
+++ Aula02/serratec/src/pages/Dashboard/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import {Resumo} from './styles';
 
 import Header from '../../components/Header';
-
-import { Container } from './styles';
+import api from '../../Services/API';
 
 const Dashboard = () => {
+  const [task, setTask] = useState([]);
+
+  // const loadTaks = async () =>{
+  //   const response = await api.get('tarefas');
+  //   setTask(response.data);
+  // }
+
+  const loadTaks = useCallback(
+    async () => {
+      const response = await api.get('tarefas');
+      setTask(response.data);
+    }, []
+  );
+
+  const task_concluded_qtd = useMemo(
+    () => {
+      const filtered = task.filter(task => {
+        return task.concluido === true;
+      })
+
+      return filtered.length;
+
+    }, [task]
+  )
+
+  const task_qtd = useMemo(
+    () => task.length, [task]
+  )
+
+  const task_pendente = useMemo(
+    () => task_qtd - task_concluded_qtd, [task_qtd, task_concluded_qtd]
+  )
+
+  useEffect(() => {
+    loadTaks();
+  }, [loadTaks]);
 
   return (
     <>
-      <Header />
-      
-      <h1>Dashboard</h1>
-      <p className="texto">ReactJS</p>
+      <Header title={'Resumo'} />
+      <Resumo>
+        { task_pendente === 0 ? (
+          <h2>Parabéns você concluiu todas as tarefas</h2>
+        ) : (
+          <h2>Existem {task_pendente} tarefas pendentes</h2>
+        )}
+      </Resumo>
+
+      <p><b>Total Tarefas: </b>{task_qtd}</p>
+      <p><b>Tarefas concluídas: </b>{task_concluded_qtd}</p>
     </>
   )
 }
--- Aula02/serratec/src/pages/Dashboard/styles.js
+++ Aula02/serratec/src/pages/Dashboard/styles.js
@@ -1,17 +1,12 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  background-color: ${props => props.ativo ? "#fff" : "#000"};
-
-  h1 {
-    color: yellow;
+export const Resumo = styled.div`
+  h2{
+    margin-bottom:20px;
+    margin-top: 20px;
   }
 
-  p {
-    font-size: 30px;
-  }
-
-  .texto{
-    color: green;
+  p{
+    line-height: 30px;
   }
 `;
